c6fa079a700321e80240e22102a0a8cd0f88fb18
wkratos22 <michaelwachnicki2001@gmail.com>
2022-06-06 17:24:34 +0200

    show finita

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -3,37 +3,27 @@
 
 @section('content')
 
-    <div>
-        <h2>NOME:
-            <span>
-                {{ $band->name }}
-            </span>
-        </h2>
-        <h4>QUANTITÀ:
-            <span>
-                {{ $band->components }}
-            </span>
-        </h4>
-        <h4>GENERE:
-            <span>
-                {{ $band->genre }}
-            </span>
-        </h4>
-        <h4>DATA E ORA:
-            <span>
-                {{ $band->live_on_stage }}
-            </span>
-        </h4>
-        <h4>DESCRIZIONE:
-            <span>
-                {{ $band->bio }}
-            </span>
-        </h4>
-        <h4>NUMERO DELLO STAGE:
-            <span>
-                {{ $band->stage_number }}
-            </span>
-        </h4>
+    <div class="container text-center mt-5">
+        <div class="card text-bg-primary mb-3 text-center mx-auto" style="max-width: 18rem;">
+            <div class="card-header"><span class="text-primary">Nome: </span>{{ $band ->name }} </div>
+            <div class="card-body">
+            <h5 class="card-title"><span class="text-primary">Componenti: </span> {{ $band->components }}</h5>
+            <p class="card-text"><span class="text-primary">Genere: </span> {{ $band->genre }}</p>
+            <p class="card-text"><span class="text-primary">Data e ora:</span> {{ $band->live_on_stage }}</p>
+            <p class="card-text"><span class="text-primary">Descrizione: </span> {{ $band->bio }}</p>
+            <p class="card-text"><span class="text-primary">Numero dello stage:</span> {{ $band->stage_number }}</p>
+            </div>
+        </div>
+
+        <div>
+            <a href="{{route ('bands.edit', $band->id)}}">edit </a>
+            <a href="{{route ('bands.remove')}}">remove </a>
+            <form action="{{route ('bands.destroy', $band->id)}} method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="submit" class="btn btn-danger" value="elimina">
+            </form>
+        </div>
     </div>
 
 @endsection

925ea443baeedeeb815209b1a370942d7ede065d
simoneluca00 <si.zaino@libero.it>
2022-06-06 17:31:24 +0200

    Delete route

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -81,8 +81,10 @@ class PageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Band $band)
     {
-        //
+        $band->delete();
+
+        return redirect()->route( 'bands.index' );
     }
 }
